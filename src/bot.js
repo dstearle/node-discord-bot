cbd7ad7e27c69f7cb82e470518603c99ce529b67
dstearle <dallas.st.earle@gmail.com>
2021-01-28 10:40:22 -0800

    created simple function that prints server messages onto the terminal

--- src/bot.js
+++ src/bot.js
@@ -6,10 +6,17 @@ const { Client } = require('discord.js');
 // New instance of 'Client' class
 const client = new Client();
 
-// Informs terminal that the bot has logged in successfully
+// Informs terminal that the bot has logged in successfully (ready event)
 client.on('ready', () => {
 
     console.log(`${client.user.tag} (${client.user.username}) has logged in!`);
+
+});
+
+// Prints server messages to the terminal (message event)
+client.on('message', (message) => {
+
+    console.log(`${message.content}`);
 
 });
 
